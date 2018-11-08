964b09eba05a0f12b0dd1748a2b9539860c873fd
Jack Li <lihz01051@gmail.com>
2018-11-08 13:27:27 -0800

    mongo compass db setup

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const mongoUri = 'mongodb://localhost/gallery';
+// const mongoUri = 'mongodb://localhost/gallery';
+const mongoUri = 'mongodb+srv://jacli1314:passwordeeznuts1@cluster0-wrvnw.mongodb.net/ONLYTHEBEST'
 
 const db = mongoose.connect(mongoUri, { useNewUrlParser: true });
-
 module.exports = db;
