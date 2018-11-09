39c6c50d5a2a61258d0febfca31c264400713801
Jack Li <lihz01051@gmail.com>
2018-11-08 17:58:48 -0800

    cloud

--- database/index.js
+++ database/index.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-const mongoUri = 'mongodb+srv://jacli1314:passwordeeznuts1@cluster0-wrvnw.mongodb.net/galleries?retryWrites=true'
-
+const mongoUri = 'mongodb+srv://jacli1314:passwordeeznuts1@cluster0-wrvnw.mongodb.net/gallery?retryWrites=true'
 mongoose.connect(mongoUri, { useNewUrlParser: true });
 const db = mongoose.connection;
 module.exports = db;
