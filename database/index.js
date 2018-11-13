8a279cb717619f8317ecb1f729ecf4e18f4af617
Hongzhong Li <lihz01051@gmail.com>
2018-11-12 16:52:47 -0800

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 // use own mongoUri
-// const mongoUri = 'mongodb+srv://jacli1314:passwordeeznuts1@cluster0-wrvnw.mongodb.net/galleries?retryWrites=true';
+const mongoUri = '';
 const db = mongoose.connect(mongoUri, { useNewUrlParser: true });
 module.exports = db;
